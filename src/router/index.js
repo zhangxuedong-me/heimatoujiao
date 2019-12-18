49df10274f4f8676b73cdbc741d729f687729960
zhangxuedong <1849310432@qq.com>
2019-12-18 23:01:47 +0800

    新建登录和主页并且配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 一级路由组件
+import homepage from '../views/home/index.vue'
+import loginpage from '../views/login/index.vue'
 Vue.use(VueRouter)
-
 const routes = [
 
+  // 一级路由导航
+  // 默认导航
+  { path: '/', redirect: '/login' },
+  // 主页路由
+  { path: '/home', component: homepage },
+  // 登录页面的路由
+  { path: '/login', component: loginpage }
 ]
 
 const router = new VueRouter({
