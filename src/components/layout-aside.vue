df0c46892642bdeb2785fd9a924cf649f7c1fe27
zhangxuedong <1849310432@qq.com>
2019-12-19 22:39:36 +0800

    主页左侧边栏完成

---
+++ src/components/layout-aside.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="aside-container">
+        <div class="title">
+            <img src="../assets/img/logo_admin.png" alt="">
+        </div>
+        <el-menu router background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+            <el-menu-item index="1" style="padding-left: 60px;">
+                首页
+            </el-menu-item>
+            <el-submenu index="2">
+                <span slot="title" style="padding-left: 40px;">内容管理</span>
+                <el-menu-item style="padding-left: 80px;">发布文章</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">内容列表</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">评论列表</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">素材管理</el-menu-item>
+             </el-submenu>
+             <el-submenu index="3">
+                <span slot="title" style="padding-left: 40px;">粉丝管理</span>
+                <el-menu-item style="padding-left: 80px;">图文数据</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">粉丝概括</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">粉丝画像</el-menu-item>
+                <el-menu-item style="padding-left: 80px;">粉丝列表</el-menu-item>
+             </el-submenu>
+              <el-menu-item index="3" style="padding-left: 60px;">
+                账户信息
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+    .aside-container {
+
+        width: 100%;
+        .title {
+
+            width: 100%;
+            height: 60px;
+            background-color: #2e2f32;
+            text-align: center;
+            img {
+
+                width: 120px;
+                margin-top: 10px;
+            }
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-      <h1>主页面</h1>
+    <el-container>
+      <el-aside style="width: 230px;height:100vh;background-color:#353b4e;">
+        <layout-aside></layout-aside>
+      </el-aside>
+
+      <el-container>
+
+        <el-header>
+          中间头部内容
+        </el-header>
+
+        <el-main>
+          <!-- 中间需要路由的部分 -->
+          <router-view></router-view>
+        </el-main>
+
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import layoutAside from '../../components/layout-aside.vue'
 export default {
 
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
 </style>
